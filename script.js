3a82e48f54d535de76edb5a9bcd950ebb6abdf30
samuelpaiva20004 <samupaivasoto@gmail.com>
2024-06-28 06:26:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+let intentos = 6;
+let diccionario = ['RATON', 'PERRO', 'COMPU', 'ESTRES']
+Math.floor(Math.random() * 10) + 1;
+const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+
+function intentar(){
+    const INTENTO = leerIntento();
+    if (INTENTO === palabra ) {
+        terminar("<h1>GANASTE!😀</h1>")
+        return
+    }
+    for (let i in palabra){
+        if (INTENTO[i]===palabra[i]){
+            console.log(INTENTO[i], "VERDE")
+        } else if( palabra.includes(INTENTO[i]) ) {
+            console.log(INTENTO[i], "AMARILLO")
+        } else {
+            console.log(INTENTO[i], "GRIS")
+        }
+    }
+		intentos--
+        if (intentos==0){
+            terminar("<h1>PERDISTE!😖</h1>")
+        }
+        const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
+    for (let i in palabra){
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if (INTENTO[i]===palabra[i]){ //VERDE
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'green';
+        } else if( palabra.includes(INTENTO[i]) ) { //AMARILLO
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'yellow';
+        } else {      //GRIS
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'grey';
+        }
+        ROW.appendChild(SPAN)
+    }
+    GRID.appendChild(ROW)
+    }
+
+        
+function leerIntento(){
+    let intento = document.getElementById("guess-input");
+    intento = intento.value;
+    intento = intento.toUpperCase(); 
+    return intento;
+}   
+function terminar(mensaje){
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+}
+
+window.addEventListener('load', init)
+function init(){
+    console.log('Esto se ejecuta solo cuando se carga la pagina web')
+}
+const button = document.getElementById("guess-button");
+button.addEventListener("click", intentar);
+const input = document.getElementById("guess-input");
+const valor = input.value;
+for (let i in palabra){
+	console.log(palabra[i]);
+}
+
